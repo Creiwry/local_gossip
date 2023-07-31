890bc124ff63c1d0e0bf5518a3a777d2b3f94517
Creiwry <jay.gardos@outlook.com>
2023-07-31 15:43:09 +0200

    layout(views): add basic css to the views

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,11 +2,3 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import 'bootstrap'
-// import * as bootstrap from "bootstrap"
-// import { Dropdown } from 'bootstrap'
-
-// document.addEventListener('turbo:load', function () {
-//   Array.from(document.querySelectorAll('[data-bs-toggle="dropdown"]')).forEach(dropdownToggle => {
-//     new Dropdown(dropdownToggle);
-//   });
-// })
