909ed47f55bdcdfb14e4797f6ef0269abdf06a2e
Creiwry <jay.gardos@outlook.com>
2023-08-03 00:02:09 +0200

    feat(controller): add user create functionality to the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,23 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-  
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(users_params)
+
+    if @user.save
+      flash[:notice] = 'Registration successful'
+      redirect_to gossips_path
+    else
+      flash[:alert] = 'Registration unsuccessful'
+      render :new
+    end
+  end
+
   def destroy
     @user = User.find(params[:id])
     if @user.destroy
@@ -17,6 +33,19 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def users_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :description,
+      :age,
+      :email,
+      :password,
+      :password_confirmation,
+      :city_id
+    )
+  end
 
   def record_not_found
     render plain: '404 Not Found', status: 404
