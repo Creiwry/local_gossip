0084f3f291d1b4b01c44ccdd34ee7392d9444950
Creiwry <jay.gardos@outlook.com>
2023-08-02 23:13:52 +0200

    feat(controller): add tags show functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
     resources :comments do
       resources :comments
     end
-    resources :tags
   end
+  resources :tags
 
   resources :users
   resources :cities, only: :show
