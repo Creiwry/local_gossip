eb4a0ba276c13e23797e85ebc1b4838378941aba
Creiwry <jay.gardos@outlook.com>
2023-11-18 21:31:26 +0100

    feat(database): add location to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   validates :city, presence: true
   validates :age, presence: true
   validates :description, presence: true
+  validates :latitude, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }, allow_nil: true
+  validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }, allow_nil: true
 
   belongs_to :city
   has_many :gossips
